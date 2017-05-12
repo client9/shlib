c15c0bf6942b1f8700987c44583f24923d0c5039
nickg <nickg@signalsciences.com>
2017-05-11 19:57:33 -0700

    public domain!

---
+++ license.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cat /dev/null << EOF
+------------------------------------------------------------------------
+client9/posixshell - portable posix shell functions
+
+See https://github.com/client9/posixshell for the latest.
+
+Public domain - http://unlicense.org
+https://github.com/client9/posixshell/blob/master/LICENSE.md
+
+but creditis (and pull requests) appreciated.
+------------------------------------------------------------------------
+EOF
