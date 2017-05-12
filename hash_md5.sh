eca5b06463ea6807bce4027fa1173988c3b9af89
nickg <nickg@signalsciences.com>
2017-05-11 18:24:10 -0700

    cleanup

--- hash_md5.sh
+++ hash_md5.sh
@@ -3,13 +3,13 @@
 # hash_md5
 #
 hash_md5 {
-    target=${@:-$(</dev/stdin)};
-    if type md5sum &> /dev/null; then
-        md5sum $target
-    elif type md5 &> /dev/null; then
-        md5 -q $target
-    else
-        echo "md5 not available 
-        return 1
-    fi
+  target=${@:-$(</dev/stdin)};
+  if type md5sum &> /dev/null; then
+    md5sum $target
+  elif type md5 &> /dev/null; then
+    md5 -q $target
+  else
+    echo "md5 not available"
+    return 1
+  fi
 }
